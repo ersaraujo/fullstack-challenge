d8eed8c8e430fbe332eb22725acf77457c9865c6
ersaraujo <ersa@cin.ufpe.br>
2025-02-27 21:10:08 -0300

    Add tests

--- backend/participants/models.py
+++ backend/participants/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 class Participant(models.Model):
     first_name = models.CharField(max_length=10)
     last_name = models.CharField(max_length=10)
     participation = models.FloatField()
 
+    def clean(self):
+        if any(char.isdigit() for char in self.first_name):
+            raise ValidationError({'first_name': 'First name cannot contain numbers.'})
+
+        if any(char.isdigit() for char in self.last_name):
+            raise ValidationError({'last_name': 'Last name cannot contain numbers.'})
+
     def __str__(self):
-        return f"{self.first_name} {self.last_name}- {self.participation}"
+        return f"{self.first_name} {self.last_name} - {self.participation}"
--- backend/participants/tests.py
+++ backend/participants/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from participants.models import Participant
+from django.core.exceptions import ValidationError
 
 # Create your tests here.
+class ParticipantModelTest(TestCase):
+
+    def test_first_name_contains_numbers(self):
+        participant = Participant(first_name="John123", last_name="Doe", participation=80.5)
+        with self.assertRaises(ValidationError):
+            participant.full_clean()  # Isso irá chamar o método clean() e validar o modelo.
+
+    def test_last_name_contains_numbers(self):
+        participant = Participant(first_name="John", last_name="Doe123", participation=80.5)
+        with self.assertRaises(ValidationError):
+            participant.full_clean()  # Isso irá chamar o método clean() e validar o modelo.
+
+    def test_valid_participant(self):
+        participant = Participant(first_name="John", last_name="Doe", participation=80.5)
+        try:
+            participant.full_clean()  # Validação bem-sucedida
+        except ValidationError:
+            self.fail("Participant should be valid!")
